6d903bd6d6fec9e886eb41facc87d06f2917ecb9
Ksenia Malyshev <kseniamalyshev@Dmitriis-MacBook-Pro.local>
2021-06-04 15:24:13 -0400

    feat: addded camera intent

--- app/src/main/java/com/example/bucketlisttravel/AddPlaceActivity.kt
+++ app/src/main/java/com/example/bucketlisttravel/AddPlaceActivity.kt
@@ -6,6 +6,7 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
@@ -28,8 +29,130 @@ import java.util.*
 
 class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
     private var cal = Calendar.getInstance()
-    private val galleryIntent =
-        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+    private lateinit var dateListener: DatePickerDialog.OnDateSetListener
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_place)
+        setSupportActionBar(toolbar_add_place)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_add_place.setNavigationOnClickListener {
+            this.onBackPressed()
+        }
+        dateListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInVIew()
+        }
+        et_date.setOnClickListener(this)
+        tv_add_image.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.et_date -> {
+                DatePickerDialog(
+                    this@AddPlaceActivity,
+                    dateListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+            R.id.tv_add_image -> {
+                val dialog = AlertDialog.Builder(this)
+                val items = arrayOf(
+                    getString(R.string.add_image_dialog_option_1),
+                    getString(R.string.add_image_dialog_option_2)
+                )
+                dialog.run {
+                    setTitle(R.string.add_image_dialog_title)
+                    setItems(items) { _, which ->
+                        when (which) {
+                            0 -> choosePhotoFromGallery()
+                            1 -> takePhotoFromCamera()
+                        }
+                    }
+                    show()
+                }
+            }
+        }
+    }
+
+    private fun takePhotoFromCamera() {
+        Dexter.withContext(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        ).withListener(
+            object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                    if (p0 != null && p0.areAllPermissionsGranted()) {
+                        val cameraIntent =
+                            Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        cameraActivity.launch(cameraIntent)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+                    showRationalDialogForPermission()
+                }
+            }
+        ).onSameThread().check()
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(
+            object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
+                    if (p0 != null && p0.areAllPermissionsGranted()) {
+                        val galleryIntent =
+                            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                        galleryActivity.launch(galleryIntent)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: MutableList<PermissionRequest>?,
+                    p1: PermissionToken?
+                ) {
+                    showRationalDialogForPermission()
+                }
+            }
+        ).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermission() {
+        AlertDialog.Builder(this)
+            .setMessage(R.string.add_image_dialog_no_permission)
+            .setPositiveButton(R.string.add_image_dialog_positive_btn) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton(R.string.add_image_dialog_negative_btn) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun updateDateInVIew() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        et_date.setText(sdf.format(cal.time).toString())
+    }
+
     private val galleryActivity =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             when (result.resultCode) {
@@ -73,105 +196,16 @@ class AddPlaceActivity : AppCompatActivity(), View.OnClickListener {
                 }
             }
         }
-    private lateinit var dateListener: DatePickerDialog.OnDateSetListener
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_place)
-        setSupportActionBar(toolbar_add_place)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        toolbar_add_place.setNavigationOnClickListener {
-            this.onBackPressed()
-        }
-        dateListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
-            cal.set(Calendar.YEAR, year)
-            cal.set(Calendar.MONTH, month)
-            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateDateInVIew()
-        }
-        et_date.setOnClickListener(this)
-        tv_add_image.setOnClickListener(this)
-    }
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.et_date -> {
-                DatePickerDialog(
-                    this@AddPlaceActivity,
-                    dateListener,
-                    cal.get(Calendar.YEAR),
-                    cal.get(Calendar.MONTH),
-                    cal.get(Calendar.DAY_OF_MONTH)
-                ).show()
-            }
-            R.id.tv_add_image -> {
-                val dialog = AlertDialog.Builder(this)
-                val items = arrayOf(
-                    getString(R.string.add_image_dialog_option_1),
-                    getString(R.string.add_image_dialog_option_2)
-                )
-                dialog.run {
-                    setTitle(R.string.add_image_dialog_title)
-                    setItems(items) { _, which ->
-                        when (which) {
-                            0 -> choosePhotoFromGallery()
-                            1 -> Toast.makeText(
-                                this@AddPlaceActivity,
-                                "Camera selection",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                    show()
+    private val cameraActivity =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            when (result.resultCode) {
+                Activity.RESULT_OK -> {
+                    val thumbnail = result.data?.extras?.get("data") as? Bitmap
+                    iv_place_image.setImageBitmap(thumbnail)
+                }
+                Activity.RESULT_CANCELED -> {
                 }
             }
         }
-    }
-
-    private fun choosePhotoFromGallery() {
-        Toast.makeText(this@AddPlaceActivity, "Gallery selection", Toast.LENGTH_SHORT).show()
-        Dexter.withContext(this).withPermissions(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ).withListener(
-            object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(p0: MultiplePermissionsReport?) {
-                    if (p0 != null && p0.areAllPermissionsGranted()) {
-                        galleryActivity.launch(galleryIntent)
-                    }
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    p0: MutableList<PermissionRequest>?,
-                    p1: PermissionToken?
-                ) {
-                    showRationalDialogForPermission()
-                }
-            }
-        ).onSameThread().check()
-    }
-
-    private fun showRationalDialogForPermission() {
-        AlertDialog.Builder(this)
-            .setMessage(R.string.add_image_dialog_no_permission)
-            .setPositiveButton(R.string.add_image_dialog_positive_btn) { _, _ ->
-                try {
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = Uri.fromParts("package", packageName, null)
-                    intent.data = uri
-                    startActivity(intent)
-                } catch (e: ActivityNotFoundException) {
-                    e.printStackTrace()
-                }
-            }
-            .setNegativeButton(R.string.add_image_dialog_negative_btn) { dialog, _ ->
-                dialog.dismiss()
-            }
-            .show()
-    }
-
-    private fun updateDateInVIew() {
-        val myFormat = "dd.MM.yyyy"
-        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
-        et_date.setText(sdf.format(cal.time).toString())
-    }
 }
